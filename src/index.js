2ef2e05b882fad25d45ad90fdca64f3ec1800500
Rushaniya <rusha359@gmail.com>
2023-01-19 12:48:54 +0300

    modified package-lock.json

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // {/* </React.StrictMode> */}
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
